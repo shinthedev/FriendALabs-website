2f9b1a7380b44a0472f94aeb5d07d3af037b8338
Shin <99096179+shinthedev@users.noreply.github.com>
2022-08-25 10:50:12 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<main class="mx-16 mt-6">
+	<h1 class="text-4xl text-indigo-500">Svelte + TailwindCSS Template</h1>
+	<p>Visit the <a class="text-indigo-700 hover:underline" href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style global>
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
